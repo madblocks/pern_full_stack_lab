1a60c13d8396e048b22cd604d23512f551750fc1
Andrew Kennedy <akennedy811@icloud.com>
2022-11-29 22:01:45 -0500

    forms

--- client/pern-movie-demo/src/components/MovieDetails.jsx
+++ client/pern-movie-demo/src/components/MovieDetails.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom'
 import styled from 'styled-components'
 import axios from 'axios'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrash, faEdit, faFileEdit, faUserEdit } from '@fortawesome/free-solid-svg-icons'
+import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons'
 
 
 const StyledMovie = styled.div`
@@ -20,12 +20,45 @@ const StyledMovie = styled.div`
     border-bottom: 1px solid rgb(171, 191, 212);
     margin-bottom: 10px;
     padding-bottom: 5px;
+    text-decoration: none;
+    outline: none;
   }
   .navLink {
     padding: 10px;
     font-size: 20px;
   }
   .navLink:hover {
+    color: white;
+  }
+  .form {
+    text-decoration: none;
+    outline: none;
+    
+  }
+  .input {
+    text-decoration: none;
+    outline: none;
+    font-size: 16px;
+  }
+  .title:disabled {
+    font-size: 25px;
+    border-bottom: 1px solid rgb(171, 191, 212);
+    box-sizing: border-box;
+    outline: none !important;
+    background-color: rgb(0, 25, 50);
+  }
+  *:disabled {
+    background-color: rgb(0, 25, 50);
+    border: none;
+    color: rgb(171, 191, 212);
+    font-size: 16px;
+  }
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+  .active {
     color: white;
   }
 `
@@ -35,11 +68,12 @@ export default function MovieDetails () {
   let { movieId } = useParams()
 
   const [ movie, setMovie ] = useState()
+  const [ editDisabled, setEditDisabled ] = useState(true)
 
   useEffect(() => {
     const getMovie = async () => {
       const response = await axios.get(`http://localhost:3001/api/movies/${movieId}`)
-      setMovie(response.data)
+      setMovie(response.data[0])
     }
     getMovie()
   }, [movieId])
@@ -49,13 +83,29 @@ export default function MovieDetails () {
     console.log(`Movie with ID: ${movieId} has been deleted`)
   }
 
+  const handleEdit = (e) => {
+    setMovie({...movie, [e.target.dataset.id]: e.target.value})
+  }
+
+  const toggleEdit = () => {
+    // const inputs = document.querySelector('.input')
+    // inputs.setAttribute('disabled', false)
+    setEditDisabled(!editDisabled)
+  }
+
   return (movie ? (
     <StyledMovie>
-      <div className='title'>{movie[0].title}</div>
-      <div className='genre'>Genre: {movie[0].genre}</div>
-      <div className='releaseDate'>Release: {movie[0].releaseDate}</div>
-      <div className='director'>Director: {movie[0].director.firstName} {movie[0].director.lastName}</div>
-      <FontAwesomeIcon className='navLink' icon={faEdit} id='editIcon'/>
+      <form className='form'>
+        <label></label><input className='title input' value={movie.title} data-id='title' onChange={handleEdit} disabled={editDisabled}/><br></br>
+        <label>Genre: </label><input className='genre input' value={movie.genre} data-id='genre' onChange={handleEdit} disabled={editDisabled}/><br></br>
+        <label>Release: </label><input className='releaseDate input' value={movie.releaseDate} data-id='releaseDate' onChange={handleEdit} disabled={editDisabled}/><br></br>
+        <label>Director: </label><input placeholder='Firstname' value={movie.director.firstName} onChange={handleEdit} disabled/><input value={movie.director.lastName} onChange={handleEdit} disabled/><br></br>
+      </form>
+      <div className='title'>{movie.title}</div>
+      <div className='genre'>Genre: {movie.genre}</div>
+      <div className='releaseDate'>Release: {movie.releaseDate}</div>
+      <div className='director'>Director: {movie.director.firstName} {movie.director.lastName}</div>
+      <FontAwesomeIcon className={editDisabled ? 'navLink' : 'navLink active'} icon={faEdit} id='editIcon' onClick={toggleEdit}/>
       <FontAwesomeIcon className='navLink' icon={faTrash} id='trashIcon' onClick={deleteMovie}/>
     </StyledMovie>
   ) : null)
